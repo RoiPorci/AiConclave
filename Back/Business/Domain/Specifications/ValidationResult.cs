c44cdb4e3cda2eed58932c1fc3e7371c6aee6ebb
RoiPorci <valerian.charron@gmail.com>
2025-03-08 18:26:26 +0100

    ♻️ Refactor entity classes
    Removed OwnedResources from the constructor and updated namespace syntax.

---
+++ Back/Business/Domain/Specifications/ValidationResult.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Business.Domain.Validators;
+
+/// <summary>
+/// Represents the result of a validation operation, including a list of errors.
+/// </summary>
+public class ValidationResult
+{
+    /// <summary>
+    /// Gets the list of validation errors.
+    /// </summary>
+    public List<string> Errors { get; } = new List<string>();
+
+    /// <summary>
+    /// Indicates whether the validation succeeded.
+    /// </summary>
+    public bool IsValid => Errors.Count == 0;
+
+    /// <summary>
+    /// Adds an error to the validation result.
+    /// </summary>
+    /// <param name="error">The error message to add.</param>
+    public void AddError(string error)
+    {
+        Errors.Add(error);
+    }
+}
+
